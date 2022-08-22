f7a23c8b31d026a408a7d016e549b1a6607f2818
Chinenye Okeke Maryrose <60973945+RoseChinenye@users.noreply.github.com>
2022-08-22 22:31:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 //testimonial slide
 
-let slideIndex=0;
-    showSlides();
+let myslideIndex = 1;
+showSlides(myslideIndex);
 
-    function showSlides(){
-        let i;
-        let slides = document.getElementsByClassName('testimonials');
-            for(i=0; i<slides.length; i++){
-                slides[i].style.display= "none";
+function activeSlide(n) {
+  showSlides(myslideIndex = n);
 }
-        slideIndex++;
-    if(slideIndex > slides.length){
-            slideIndex = 1;
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("testimonials");
+  let dot = document.getElementsByClassName("dots");
+  if (n > slides.length) {myslideIndex = 1}    
+  if (n < 1) {myslideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  slides[myslideIndex-1].style.display = "block";  
+  dot[myslideIndex-1].className += " active";
 }
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000);
-}
+
+//hamburger menu
+
+const burger = document.querySelector(".burger");
+const nav_links = document.querySelector(".navlinks");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    nav_links.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() => {
+    burger.classList.remove("active");
+    nav_links.classList.remove("active");
+}))
